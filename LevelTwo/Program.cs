8dcfa0492125919060d8eda6d1bf6bab16d9523e
Lisa Carvalho <i.m.lisaaa14@gmail.com>
2025-03-21 13:50:16 +0000

    Add Print Tux

---
+++ LevelTwo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Spectre.Console;
+
+namespace LevelTwo
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            CanvasImage image = new CanvasImage(args[0]);
+            image.MaxWidth(int.Parse(args[1]));
+
+            if (args == null )
+            {
+                image = new CanvasImage("tux.jpg");
+                image.MaxWidth(24);
+                AnsiConsole.Write(image);
+
+            }
+            else 
+            {
+                AnsiConsole.Write(image);
+            }
+        }
+    }
+}
